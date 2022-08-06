c1f43381d0f095608e68e9fc3676acd8b0607599
JHorlamide <olamidejubril68@gmail.com>
2022-08-06 18:32:13 +0100

    Setting up store

---
+++ src/views/CartPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Cart</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartPage",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/ProductPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+   <ProductList />
+  </div>
+</template>
+
+<script>
+import ProductList from "@/components/ProductList.vue";
+export default {
+  name: "ProductPage",
+  components: { ProductList },
+};
+</script>
